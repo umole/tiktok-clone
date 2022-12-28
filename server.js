0b62b5976c8655fc586879bbfc38c7711bf4a8ff
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-12-28 02:56:35 +0100

    A full stack clone of the zoom app

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+})
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`);
+})
